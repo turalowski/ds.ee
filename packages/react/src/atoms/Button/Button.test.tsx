91233d7409c189994b467d26048322a93ae654b3
Tural Hajiyev <thajiyev9@outlook.com>
2024-11-18 22:32:08 +0100

    fix(ts): fixed ts errors in test file

--- packages/react/src/atoms/Button/Button.test.tsx
+++ packages/react/src/atoms/Button/Button.test.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import { render, fireEvent } from '@testing-library/react';
+import '@testing-library/jest-dom';
 import Button from './Button';
 
 describe('Button', () => {
