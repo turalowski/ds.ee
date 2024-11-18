1537e0567c32c22316a9ab17ac7af183227ab043
Tural Hajiyev <thajiyev9@outlook.com>
2024-11-18 22:22:45 +0100

    feat(select component): deleted select component

--- packages/react/src/index.ts
+++ packages/react/src/index.ts
@@ -1,9 +1,8 @@
 import Color from './atoms/Color';
 import Text from './atoms/Text';
 import Margin from './atoms/Margin';
-import Select from './molecules/Select';
 import Button from './atoms/Button';
 import Input from './atoms/Input';
 import Typography from './atoms/Typography'
 
-export { Text, Margin, Color, Select, Button, Input, Typography };
+export { Text, Margin, Color, Button, Input, Typography };
